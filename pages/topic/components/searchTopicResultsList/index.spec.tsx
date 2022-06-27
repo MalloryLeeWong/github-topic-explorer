4fd61c1a0083c6decc2dac72ddc3793e0579690d
Mallory LeeWong <m.c.leewong@gmail.com>
2022-06-27 13:56:56 -0400

    test: install react-test-renderer and types and set up basic test

--- pages/topic/components/searchTopicResultsList/index.spec.tsx
+++ pages/topic/components/searchTopicResultsList/index.spec.tsx
@@ -1,7 +1,29 @@
-import SearchTopicResultsList from './index'
+import { SearchTopicResultsList } from './index';
+import TestRenderer from 'react-test-renderer';
+
+const TEST_PROPS = {
+  handleClickRelatedTopic: jest.fn(),
+  isLoading: false,
+  relatedTopics: [
+    { id: '2', name: 'cheddar', stargazers: { totalCount: 10 } },
+    { id: '3', name: 'pizza', stargazers: { totalCount: 20 } },
+    { id: '4', name: 'delicious', stargazers: { totalCount: 30 } },
+  ],
+};
 
 describe('SearchTopicResultsList', () => {
   it('returns a list', () => {
-    console.log('test')
-  })
-})
+    const testRenderer = TestRenderer.create(
+      <SearchTopicResultsList
+        isLoading={TEST_PROPS.isLoading}
+        relatedTopics={TEST_PROPS.relatedTopics}
+        handleClickRelatedTopic={TEST_PROPS.handleClickRelatedTopic}
+      />
+    );
+
+    const testInstance = testRenderer.root;
+
+    const elementList = testInstance.findAllByType('li');
+    expect(elementList.length).toEqual(3);
+  });
+});
