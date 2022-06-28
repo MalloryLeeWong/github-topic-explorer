f7a3c3c28b2d34948aaf769beff8e8218ea06322
Mallory LeeWong <m.c.leewong@gmail.com>
2022-06-28 00:25:03 -0400

    docs: clarify comments

--- apollo-client.js
+++ apollo-client.js
@@ -4,7 +4,9 @@ const client = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   cache: new InMemoryCache(),
   headers: {
-    // You must add your own token for the GitHub API below
+    /* You must add your own token for the GitHub API below
+     * Docs: https://docs.github.com/en/graphql/guides/forming-calls-with-graphql#authenticating-with-graphql
+     */
     authorization: `Bearer YOUR_GITHUB_API_KEY`,
   },
 });
